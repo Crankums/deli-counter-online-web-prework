f130b929f535626c07badac944b998427fb014aa
Craig Donavin <crdonavin@gmail.com>
2019-03-24 14:59:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ def take_a_number(katz_deli, name)
   if katz_deli.length < 1
     katz_deli << name
   end
- puts "Welcome, #{name}. You are number #{} in line."
+ puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving
