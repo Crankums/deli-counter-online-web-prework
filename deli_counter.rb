744a771339b1effeac52037f02b96d995b47e235
Craig Donavin <crdonavin@gmail.com>
2019-03-24 14:17:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,8 @@ def line(arr)
   str = "The line is currently: "
   if arr.length < 1
     puts "The line is currently empty."
-  else arr.each do |i|
-    str << "#{count}. #{i}"
+  else arr.each { |i|
+    str << "#{count}. #{i}", count +=1 }
     puts str
   end
   end
